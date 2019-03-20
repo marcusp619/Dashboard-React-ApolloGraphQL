48f4de5bf5d8d367e29e0151c2e0c9d418c73cdd
marcusp619 <marcusp619@gmail.com>
2019-03-20 17:40:04 -0600

    Add apollo provider to app

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ import 'typeface-roboto';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql'
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
