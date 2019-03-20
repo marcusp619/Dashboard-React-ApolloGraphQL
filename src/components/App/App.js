0a10b3a95a53efaadeccaeee6746c0bfeb17101c
marcusp619 <marcusp619@gmail.com>
2019-03-19 23:00:58 -0600

    Add navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import NavBar from '../NavBar';
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div>
+        <NavBar />
+      </div>
+    );
   }
 }
 
--- src/components/AppBar/index.js
+++
@@ -1,5 +0,0 @@
-import react, { Component } from 'react';
-
-class AppBar extends Component {}
-
-export default AppBar;
